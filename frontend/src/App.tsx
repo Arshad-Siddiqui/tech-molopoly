8d460680b63976100e71c7826b7968bbcba9509a
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-02 18:02:23 +0000

    Add routes and a homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import NavBar from "./components/NavBar/NavBar"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div className="App">
       <NavBar title='Tech Monopoly!' />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
