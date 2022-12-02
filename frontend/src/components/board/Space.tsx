0ac2dadd5afdc6802f09ea020717ed5cb548063d
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-02 19:03:19 +0000

    Rename BoardCard to Space

---
+++ frontend/src/components/board/Space.tsx
@@ -0,0 +1,19 @@
+const Space = (card: Card) => {
+  return (
+    <div className='space'>
+      <div className={'space-header' + ` ${card.color}`} />
+      <div className='space-body'>
+        <p className='space-name'>{card.name}</p>
+        <p className='space-price'>${card.price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Space;
+
+interface Card {
+  name: string;
+  color: string | null;
+  price: number;
+}
