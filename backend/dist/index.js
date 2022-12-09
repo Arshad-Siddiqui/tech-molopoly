0fa731a2c058dfea3e508e2fe2ac2b79a6b30e3b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-09 14:51:59 +0000

    Add websocket boilerplate

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,9 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
