49c8e59b3953a3f6b709d0c21699a649ef8f0bd7
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-13 11:10:43 +0000

    Increase card size

--- frontend/src/components/Cards/Property.tsx
+++ frontend/src/components/Cards/Property.tsx
@@ -9,7 +9,13 @@ export default function Property({ property }: { property: Card }) {
         <p className="title-deed">Title Deed</p>
         <p className="property-name">{property.name}</p>
       </div>
-      <div className="property-rent">rent {property.price}</div>
+      <div className="property-rent">rent {property.price}M</div>
+      <div className="property-wh">
+        <div className="property-rent-wh house1">rent with house {property.house1}</div>
+        <div className="property-rent-wh house2">rent with house {property.house2}</div>
+        <div className="property-rent-wh house3">rent with house {property.house3}</div>
+        <div className="property-rent-wh house4">rent with house {property.house4}</div>
+      </div>
       <div className="property-rent-ws">rent with colour set {property.rent}</div>
     </div>
   );
