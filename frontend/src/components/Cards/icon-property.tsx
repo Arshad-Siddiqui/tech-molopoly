167d2a29163c8a88b9b9182e4201dd1b3596e40c
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-18 20:44:01 +0000

    Add font-awesome

---
+++ frontend/src/components/Cards/icon-property.tsx
@@ -0,0 +1,7 @@
+import Property from "../../types/property";
+
+export default function IconProperty({ property }: { property: Property }) {
+  const iconMap = {};
+
+  return <h1>icon property</h1>;
+}
