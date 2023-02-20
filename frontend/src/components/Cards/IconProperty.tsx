119875b4962082f5f38946ec0a7fd363ba034b73
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-20 12:51:48 +0000

    Standardize class names

--- frontend/src/components/Cards/IconProperty.tsx
+++ frontend/src/components/Cards/IconProperty.tsx
@@ -2,8 +2,8 @@ import Property from "../../types/property";
 
 export default function IconProperty({ property }: { property: Property }) {
   return (
-    <div className="icon-property">
-      <div className="icon-property__name">{property.name}</div>
+    <div className="icon-property card">
+      <div className="icon-property-name">{property.name}</div>
       <div className="icon-property-image">FA-ICON goes here</div>
       <div className="icon-property-price">{property.price}</div>
     </div>
