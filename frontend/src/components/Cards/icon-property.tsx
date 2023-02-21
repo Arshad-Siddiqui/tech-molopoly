1c1acb12de021b238250fe78fd2b4003d709e88e
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-21 14:45:21 +0000

    Normalize IProp header height

--- frontend/src/components/Cards/icon-property.tsx
+++ frontend/src/components/Cards/icon-property.tsx
@@ -3,9 +3,11 @@ import Property from "../../types/property";
 export default function IconProperty({ property }: { property: Property }) {
   return (
     <div className="icon-property card">
-      <div className="icon-property-name property-name">{property.name}</div>
-      <div className="icon-property-image">FA-ICON goes here</div>
-      <div className="icon-property-price">{property.price}</div>
+      <div className="icon-property-header">
+        <div className="icon-property-name property-name">{property.name}</div>
+      </div>
+      <div className="icon-property-image-container">FA-ICON goes here</div>
+      <div className="icon-property-price">M{property.price}</div>
     </div>
   );
 }
