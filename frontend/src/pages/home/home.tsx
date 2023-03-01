176eba359a9168475fbe82b6dad0d972a19c3a30
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-03-01 11:51:43 +0000

    Add play icon

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import "./home.css";
 
 export default function Home() {
   return (
     <div className="home-container">
       <h1>Home</h1>
-      <Link to="/play">Play!</Link>
+      <Link to="/play">
+        <h2 className="controller-icon">
+          <FontAwesomeIcon icon={faGamepad} />
+        </h2>
+      </Link>
     </div>
   );
 }
