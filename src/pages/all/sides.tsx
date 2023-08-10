95af1634747bfa972b59addfd849af4628b036d9
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-08-10 23:17:25 +0100

    Get all properties onto their side

--- src/pages/all/sides.tsx
+++ src/pages/all/sides.tsx
@@ -5,12 +5,21 @@ export default function AllSides({ properties }: { properties: Properties[] }) {
   // Starting with one side
 
   const bottomProperties = properties.slice(0, 5);
+  const leftProperties = properties.slice(5, 10);
+  const topProperties = properties.slice(10, 15);
+  const rightProperties = properties.slice(15, 20);
 
   return (
     <div className="all-sides">
       <h1>All Sides</h1>
-      <h2>Top Side</h2>
+      <h2>Bottom Side</h2>
       <Side properties={bottomProperties} position="bottom" />
+      <h2>Left Side</h2>
+      <Side properties={leftProperties} position="left" />
+      <h2>Top Side</h2>
+      <Side properties={topProperties} position="top" />
+      <h2>Right Side</h2>
+      <Side properties={rightProperties} position="right" />
     </div>
   );
 }
